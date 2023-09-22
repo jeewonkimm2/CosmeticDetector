4e163bda1fc58655ab02ae7d9892b1d9422f9499
LBC <wpdltm4@gmail.com>
2023-09-22 21:14:55 +0900

    enhance: Include product price during scraping process
    [issue] #1

--- cosmetics_crawler.py
+++ cosmetics_crawler.py
@@ -1,7 +1,7 @@
 from brand_urls_scraper import fetch_brand_urls
 from products_list_loader import load_existing_products
 from brand_products_scraper import fetch_brand_and_product_info
-from product_photo_urls_scraper import create_dataframe_with_photos
+from product_photo_urls_scraper import create_dataframe_with_photos_and_price
 from product_photo_downloader import save_to_csv, save_product_images_from_df
 
 brand_urls = fetch_brand_urls()
@@ -11,7 +11,7 @@ for key in brand_urls:
     brand_links = brand_urls[key]
     brand_info_df = fetch_brand_and_product_info(brand_links, existing_products)
 
-    new_df = create_dataframe_with_photos(brand_info_df)
+    new_df = create_dataframe_with_photos_and_price(brand_info_df)
 
     save_to_csv(new_df, "products_list.csv")
     save_product_images_from_df(new_df)
--- product_photo_urls_scraper.py
+++ product_photo_urls_scraper.py
@@ -3,37 +3,36 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # detail_url에서 product photo url scraping
-def get_product_photo_urls(product_detail_url):
-    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+def get_product_details(product_detail_url):
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+    }
     response = requests.get(url=product_detail_url, headers=headers, timeout=10)
 
     soup = BeautifulSoup(response.content, 'lxml')
     picture_elements = soup.select("picture.css-4rx3j2 > source")
 
-    photo_urls = []
+    photo_urls = [element.get('srcset').split('?')[0].split(' ')[0] for element in picture_elements]
 
-    for element in picture_elements:
-        srcset = element.get('srcset')
+    # 가격 정보 가져오기
+    price_element = soup.select_one("span.css-18jtttk > b.css-0")
+    price = price_element.text if price_element else None
 
-        photo_url = srcset.split('?')[0].split(' ')[0]
-        photo_urls.append(photo_url)
+    return photo_urls, price
 
-    return photo_urls
-
-# 모든 정보가 저장된 df 생성
-def create_dataframe_with_photos(df):
-    new_columns = ['brand_name', 'product_name', 'product_detail_url', 'product_photo_urls']
+def create_dataframe_with_photos_and_price(df):
+    new_columns = ['brand_name', 'product_name', 'product_detail_url', 'product_photo_urls', 'product_price']
     new_df = pd.DataFrame(columns=new_columns)
 
     for index, row in df.iterrows():
         product_name = row['product_name']
         brand_name = row['brand_name']
         product_detail_url = row['product_detail_url']
-        
-        photo_urls = get_product_photo_urls(product_detail_url)
-        
+
+        photo_urls, price = get_product_details(product_detail_url)
+
         # photo_urls의 요소가 1개 이상일 경우에만 new_df에 추가
         if len(photo_urls) > 0:
-            new_df.loc[index] = [brand_name, product_name, product_detail_url, photo_urls]
-        
+            new_df.loc[index] = [brand_name, product_name, product_detail_url, photo_urls, price]
+
     return new_df
