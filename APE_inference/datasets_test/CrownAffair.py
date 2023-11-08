808ccc6f86cb1c725baaa21369ac196947708a36
LBC <wpdltm4@gmail.com>
2023-11-08 14:06:40 +0900

    feat: Add the modules for the private flask server

---
+++ APE_inference/datasets_test/CrownAffair.py
@@ -0,0 +1,41 @@
+
+import os
+import math
+import random
+from collections import defaultdict
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from torchvision.datasets import ImageFolder
+from torchvision import datasets
+
+class_name = 'CrownAffair'
+
+classes = 'CrownAffair'
+
+templates = [
+    "a photo of {}.",
+    "a CrownAffair photo of the {}.",
+    "a photo of the {}.",
+]
+
+class CrownAffair:
+    
+    dataset_dir = class_name
+
+    def __init__(self, root, num_shots, preprocess):
+        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.image_dir = os.path.join(self.dataset_dir, 'images')
+
+        train_preprocess = transforms.Compose([
+            transforms.RandomResizedCrop(size=224, scale=(0.5, 1), interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))])
+        test_preprocess = preprocess
+
+        # self.train = ImageFolder(f'./data/cosmetic/images/train/CrownAffair', transform=train_preprocess)
+        # self.val = ImageFolder(f'./data/cosmetic/images/val/CrownAffair', transform=test_preprocess)
+        self.test = ImageFolder(f'./data/cosmetic/images/test/CrownAffair', transform=test_preprocess)
---
+++ APE_inference/datasets_test/LYSBeauty.py
@@ -0,0 +1,41 @@
+
+import os
+import math
+import random
+from collections import defaultdict
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from torchvision.datasets import ImageFolder
+from torchvision import datasets
+
+class_name = 'LYSBeauty'
+
+classes = 'LYSBeauty'
+
+templates = [
+    "a photo of {}.",
+    "a LYSBeauty photo of the {}.",
+    "a photo of the {}.",
+]
+
+class LYSBeauty:
+    
+    dataset_dir = class_name
+
+    def __init__(self, root, num_shots, preprocess):
+        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.image_dir = os.path.join(self.dataset_dir, 'images')
+
+        train_preprocess = transforms.Compose([
+            transforms.RandomResizedCrop(size=224, scale=(0.5, 1), interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))])
+        test_preprocess = preprocess
+
+        # self.train = ImageFolder(f'./data/cosmetic/images/train/LYSBeauty', transform=train_preprocess)
+        # self.val = ImageFolder(f'./data/cosmetic/images/val/LYSBeauty', transform=test_preprocess)
+        self.test = ImageFolder(f'./data/cosmetic/images/test/LYSBeauty', transform=test_preprocess)
