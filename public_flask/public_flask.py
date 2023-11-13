f90653ca5bfde97ec89c2f19aaf2964b80d0953d
LBC <wpdltm4@gmail.com>
2023-11-13 19:53:15 +0900

    fix: init the class in the static area

--- public_flask/public_flask.py
+++ public_flask/public_flask.py
@@ -15,15 +15,12 @@ from flask_cors import CORS
 
 def prepare_environment():
     # 환경 설정 및 brand 리스트 준비
-    image_processor = ImageProcessor()
     csv_path = 'products_list.csv'
     brand_list = image_processor.extract_unique_brands(csv_path)
     return brand_list, csv_path
 
 
 def process_video(youtube_link):
-    # 비디오 처리
-    video_manager = VideoManager()
     save_path = video_manager.unique_folder()
     video_path = video_manager.download_video(youtube_link, save_path)
     threshold = 6.75
@@ -33,8 +30,6 @@ def process_video(youtube_link):
 
 
 def extract_frames(video_path, save_path, transition_times):
-    # 프레임 추출
-    video_manager = VideoManager()
     save_dir = os.path.join(save_path, 'frames')
     video_manager.save_frames_from_times(
         video_path, transition_times, save_dir)
@@ -52,24 +47,18 @@ def detect_brands_in_frames(save_path):
 
 
 def extract_and_compare_brands(youtube_link, brand_list, cropped_images_folder_path, transition_times):
-    # 브랜드 추출 및 비교
-    imageExtractor = ImageBrandExtractor()
     brand_in_image = imageExtractor.extract_and_find_brand(
         cropped_images_folder_path, brand_list)
 
-    transcriptExtractor = TranscriptBrandExtractor()
     brand_in_transcript = transcriptExtractor.extract_brands_from_youtube_url(
         youtube_link, brand_list)
 
-    image_processor = ImageProcessor()
     brand_with_image = image_processor.update_brand_in_image(
         brand_in_image, brand_in_transcript, transition_times)
     return brand_with_image
 
 
 def cleanup_and_upload(save_path, brand_with_image, cropped_images_folder_path):
-    # 정리 및 업로드
-    image_processor = ImageProcessor()
     class_names = list({item['brand_name'] for item in brand_with_image})
 
     print(class_names)
@@ -77,7 +66,6 @@ def cleanup_and_upload(save_path, brand_with_image, cropped_images_folder_path):
         brand_with_image, cropped_images_folder_path, save_path)
     image_processor.delete_except_APE_folder(save_path)
 
-    s3_utils = S3Utils()
     bucket_name = 'cosmetic-detector-bucket'
     s3_directory = save_path
     s3_utils.upload_directory_to_s3(save_path, bucket_name, s3_directory)
@@ -85,15 +73,12 @@ def cleanup_and_upload(save_path, brand_with_image, cropped_images_folder_path):
 
 
 def perform_api_inference(save_path, class_names, csv_path):
-    # API를 사용하여 추론 수행
-    API_communicator = APICommunicator()
     api_url = "http://10.100.20.170:8000/find_cosmetic"
     response_data = API_communicator.send_post_request_to_api(
         api_url, save_path, class_names)
 
     print(response_data)
 
-    data_handler = DataHandler()
     product_info_list = data_handler.read_csv_to_list(csv_path)
     result = data_handler.get_matching_products(
         response_data, product_info_list)
@@ -101,7 +86,6 @@ def perform_api_inference(save_path, class_names, csv_path):
 
 
 def analyze_video(youtube_link):
-    # 비디오 분석 주 함수
     brand_list, csv_path = prepare_environment()
     save_path, video_path, transition_times = process_video(
         youtube_link)
@@ -114,9 +98,7 @@ def analyze_video(youtube_link):
         save_path, brand_with_image, cropped_images_folder_path)
     result = perform_api_inference(save_path, class_names, csv_path)
 
-    # 후처리: 로컬 및 S3 정리
     shutil.rmtree(save_path)
-    s3_utils = S3Utils()
     bucket_name = 'cosmetic-detector-bucket'
     s3_utils.delete_files_in_s3_directory(bucket_name, save_path)
 
@@ -127,10 +109,17 @@ def analyze_video(youtube_link):
 app = Flask(__name__)
 CORS(app)
 
+image_processor = ImageProcessor()
+video_manager = VideoManager()
+API_communicator = APICommunicator()
+data_handler = DataHandler()
+imageExtractor = ImageBrandExtractor()
+transcriptExtractor = TranscriptBrandExtractor()
+s3_utils = S3Utils()
+
 
 @app.route('/analyze', methods=['POST'])
 def analyze_youtube_video():
-    # 비디오 분석 Flask 라우트
     data = request.get_json()
     youtube_link = data.get('youtube_link')
     if not youtube_link:
